a9f726c2c4499331519a0dfc6ed32195d18165d3
Lintik <domandominguez@hotmail.com>
2017-06-24 13:52:57 -0700

    Add project files.

---
+++ E-vents/E_vents/Views/AboutPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿
+using Xamarin.Forms;
+
+namespace E_vents.Views
+{
+	public partial class AboutPage : ContentPage
+	{
+		public AboutPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ E-vents/E_vents/Views/NewItemPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using E_vents.Models;
+
+using Xamarin.Forms;
+
+namespace E_vents.Views
+{
+	public partial class NewItemPage : ContentPage
+	{
+		public Item Item { get; set; }
+
+		public NewItemPage()
+		{
+			InitializeComponent();
+
+			Item = new Item
+			{
+				Text = "Item name",
+				Description = "This is a nice description"
+			};
+
+			BindingContext = this;
+		}
+
+		async void Save_Clicked(object sender, EventArgs e)
+		{
+			MessagingCenter.Send(this, "AddItem", Item);
+			await Navigation.PopToRootAsync();
+		}
+	}
+}
